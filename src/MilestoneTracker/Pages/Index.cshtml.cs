cd6f8b04282ecd3f9a0244e1a692a9df56eaaeb0
Artak <34246760+mkArtakMSFT@users.noreply.github.com>
2018-03-14 19:43:38 -0700

    Implemented OAuth-based authorization (#3)
    * Implemented OAuth authorization with GitHub
    
    * Removed Authorize page as is not needed
    
    * Fixed the styling and authorization token persistance logic

--- src/MilestoneTracker/Pages/Index.cshtml.cs
+++ src/MilestoneTracker/Pages/Index.cshtml.cs
@@ -3,9 +3,11 @@ using GitHub.Client;
 using GitHubMilestoneEstimator.Options;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 using MilestoneTracker.Model;
+using MilestoneTracker.Options;
+using Octokit;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -17,33 +19,76 @@ namespace MilestoneTracker.Pages
     {
         private const char milestoneSeparatorCharacter = ';';
 
+        private static readonly Random random = new Random(DateTime.UtcNow.Millisecond);
+        private const string AccessTokenKey = "CSRF:State";
+
         private readonly GitHubOptions gitHubOptions;
-        private readonly IWorkEstimator workEstimator;
+        private readonly GitHubAuthOptions authOptions;
+        private readonly WorkEstimatorFactory workEstimatorFactory;
+        private readonly GitHubClient client;
 
         [FromQuery]
         public string Milestone { get; set; }
 
-        public IEnumerable<string> Milestones { get => this.Milestone?.Split(milestoneSeparatorCharacter); }
+        public IEnumerable<string> Milestones { get => this.Milestone?.Split(milestoneSeparatorCharacter, StringSplitOptions.RemoveEmptyEntries).Select(item => item.Trim()); }
 
         public WorkDataViewModel Work { get; set; }
 
         public string[] TeamMembers { get => this.gitHubOptions.TeamMembers; }
 
-        public IndexModel(IWorkEstimator workEstimator, GitHubOptions gitHubOptions)
+        public IndexModel(WorkEstimatorFactory workEstimatorFactory, GitHubOptions gitHubOptions, IOptions<GitHubAuthOptions> authOptions)
         {
             this.gitHubOptions = gitHubOptions.Ensure(nameof(gitHubOptions)).IsNotNull().Value;
-            this.workEstimator = workEstimator.Ensure(nameof(workEstimator)).IsNotNull().Value;
+            this.authOptions = authOptions.Ensure(o => o.Value).IsNotNull().Value;
+            this.workEstimatorFactory = workEstimatorFactory.Ensure(nameof(workEstimatorFactory)).IsNotNull().Value;
+
+            this.client = new GitHubClient(new ProductHeaderValue(gitHubOptions.Organization), new Uri("https://github.com/"));
         }
 
-        public async Task OnGet()
+        public async Task<IActionResult> OnGet()
         {
             if (this.Milestones != null)
             {
-                await this.RetrieveWorkloadAsync();
+                IWorkEstimator workEstimator = GetWorkEstimator();
+                if (workEstimator == null)
+                {
+                    return Redirect(this.GetOauthLoginUrl());
+                }
+
+                try
+                {
+                    await this.RetrieveWorkloadAsync(workEstimator);
+                }
+                catch (AuthorizationException)
+                {
+                    return Redirect(this.GetOauthLoginUrl());
+                }
             }
+
+            return Page();
         }
 
-        private async Task RetrieveWorkloadAsync()
+        [HttpGet]
+        // This is the Callback URL that the GitHub OAuth Login page will redirect back to.
+        public async Task<IActionResult> OnGetAuthorize(string code, string state)
+        {
+            if (!String.IsNullOrEmpty(code))
+            {
+                var expectedState = TempData.Peek(AccessTokenKey) as string;
+                if (state != expectedState)
+                    throw new InvalidOperationException("SECURITY FAIL!");
+
+                TempData[AccessTokenKey] = null;
+
+                var token = await client.Oauth.CreateAccessToken(
+                    new OauthTokenRequest(this.authOptions.ClientId, this.authOptions.ClientSecret, code));
+                TempData["OAuthToken"] = token.AccessToken;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private async Task RetrieveWorkloadAsync(IWorkEstimator workEstimator)
         {
             this.Work = new WorkDataViewModel();
 
@@ -58,7 +103,7 @@ namespace MilestoneTracker.Pages
                         {
                             if (!tokenSource.IsCancellationRequested)
                             {
-                                this.Work[member, milestone] = await this.workEstimator.GetAmountOfWorkAsync(member, milestone, tokenSource.Token);
+                                this.Work[member, milestone] = await workEstimator.GetAmountOfWorkAsync(member, milestone, tokenSource.Token);
                             }
                         }));
                     }
@@ -66,6 +111,49 @@ namespace MilestoneTracker.Pages
 
                 await Task.WhenAll(tasks);
             }
+        }
+
+        private IWorkEstimator GetWorkEstimator()
+        {
+            var accessToken = TempData.Peek("OAuthToken") as string;
+            if (accessToken != null)
+            {
+                // This allows the client to make requests to the GitHub API on the user's behalf
+                // without ever having the user's OAuth credentials.
+                return this.workEstimatorFactory.Create(accessToken);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private string GetOauthLoginUrl()
+        {
+            string csrf = GenerateRandomString(24);
+            TempData[AccessTokenKey] = csrf;
+
+            // 1. Redirect users to request GitHub access
+            var request = new OauthLoginRequest(this.authOptions.ClientId)
+            {
+                Scopes = { "user", "notifications" },
+                State = csrf,
+                //RedirectUri = new Uri($"{this.Request.Scheme}://{this.Request.Host}{this.Request.Path}{this.Request.QueryString}&handler=authorize")
+            };
+            var oauthLoginUrl = client.Oauth.GetGitHubLoginUrl(request);
+            return oauthLoginUrl.ToString();
+        }
+
+        private static string GenerateRandomString(int length)
+        {
+            var passChars = new char[length];
+            var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+            for (int i = 0; i < length; i++)
+            {
+                passChars[i] = characters[random.Next(0, characters.Length - 1)];
+            }
+
+            return new string(passChars);
         }
     }
 }
