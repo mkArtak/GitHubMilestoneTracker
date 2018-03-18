4b9ffa9ca9ccd2540af54f633cd53bf305fe50e5
Artak Mkrtchyan <artakm@corp.microsoft.com>
2018-03-18 14:59:02 -0700

    Added a data layer project and partiallly implemented the teams data manager

---
+++ src/GitHubMilestoneTracker.Contracts/PagedDataResponse.cs
@@ -0,0 +1,23 @@
+﻿using AM.Common.Validation;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MilestoneTracker.Contracts
+{
+    public class PagedDataResponse<T>
+    {
+        private readonly IEnumerable<T> results;
+        private readonly string continuationToken;
+
+        public IEnumerable<T> Results { get => this.results; }
+
+        public string ContinuationToken { get => this.continuationToken; }
+
+        public PagedDataResponse(IEnumerable<T> results, string continuationToken)
+        {
+            this.results = new ReadOnlyCollection<T>(results.Ensure(nameof(results)).IsNotNull().Value.ToList());
+            this.continuationToken = continuationToken;
+        }
+    }
+}
