787f5a7faa4cb22fffbcdbc46ae22ed1f125ea57
Artak Mkrtchyan <artakm@corp.microsoft.com>
2018-03-22 16:00:54 -0700

    Integrated Google Chart js libraries through BurndownChart view

---
+++ src/MilestoneTracker/Model/BurndownChartModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MilestoneTracker.Model
+{
+    public class BurndownChartModel
+    {
+        public IEnumerable<string> Milestones { get; set; }
+
+        public string TeamName { get; set; }
+    }
+}
---
+++ src/MilestoneTracker/Model/WorkDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MilestoneTracker.Model
+{
+    public class WorkDTO
+    {
+        public string Milestone { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double DaysOfWorkLeft { get; set; }
+    }
+}
