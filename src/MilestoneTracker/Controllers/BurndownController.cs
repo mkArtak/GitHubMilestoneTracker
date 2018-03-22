de1a27a954bb2f74530de27b97a50b84ab09c3fc
Artak Mkrtchyan <artakm@corp.microsoft.com>
2018-03-22 16:47:12 -0700

    Fixed the javascript ajax post call to the service

--- src/MilestoneTracker/Controllers/BurndownController.cs
+++ src/MilestoneTracker/Controllers/BurndownController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MilestoneTracker.Model;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MilestoneTracker.Controllers
@@ -8,21 +10,22 @@ namespace MilestoneTracker.Controllers
     [Route("api/Burndown")]
     public class BurndownController : Controller
     {
-        [HttpGet]
-        public IActionResult GetBurndown()
-        {
-            return Ok();
-        }
-
         [HttpPost]
-        public async Task<IActionResult> GetBurndownDataAsync([FromQuery]string teamName/*, [FromBody]string[] milestones*/)
+        public async Task<IActionResult> GetBurndownDataAsync([FromQuery]string teamName, [FromBody]IEnumerable<string> milestones)
         {
-            string milestone1 = "2.1.0-preview2";
+            if (!ModelState.IsValid)
+            {
+                throw new InvalidOperationException(ModelState.Values.First().Errors.First().ErrorMessage);
+            }
+
+            string milestone = milestones.FirstOrDefault();
             WorkDTO[] burnDownDatas = new[] {
-                new WorkDTO { Date = new DateTime(2018, 01, 01), Milestone = milestone1, DaysOfWorkLeft = 10 },
-                new WorkDTO { Date = new DateTime(2018, 01, 02), Milestone = milestone1, DaysOfWorkLeft = 9 },
-                new WorkDTO { Date = new DateTime(2018, 01, 03), Milestone = milestone1, DaysOfWorkLeft = 10},
-                new WorkDTO { Date = new DateTime(2018, 01, 04), Milestone = milestone1, DaysOfWorkLeft = 7 }};
+                new WorkDTO { Date = new DateTime(2018, 01, 01), Milestone = milestone, DaysOfWorkLeft = 10 },
+                new WorkDTO { Date = new DateTime(2018, 01, 02), Milestone = milestone, DaysOfWorkLeft = 9 },
+                new WorkDTO { Date = new DateTime(2018, 01, 03), Milestone = milestone, DaysOfWorkLeft = 10},
+                new WorkDTO { Date = new DateTime(2018, 01, 04), Milestone = milestone, DaysOfWorkLeft = 8 },
+                new WorkDTO { Date = new DateTime(2018, 01, 06), Milestone = milestone, DaysOfWorkLeft = 7},
+                new WorkDTO { Date = new DateTime(2018, 01, 08), Milestone = milestone, DaysOfWorkLeft = 5 }};
             await Task.CompletedTask;
             return new JsonResult(burnDownDatas);
         }
