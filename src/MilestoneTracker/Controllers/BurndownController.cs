d938dfec2342c034057e88bc6f72999272e5fcd4
Artak <34246760+mkArtakMSFT@users.noreply.github.com>
2018-03-22 18:31:57 -0700

    Merged charting related changes (#6)
    * Added logic to differentiate between min and max work amount data in the rendered table
    
    * Added a data layer project and partiallly implemented the teams data manager
    
    * Added a data layer project and partiallly implemented the teams data manager
    
    * Storing changes
    
    * Second set of changes to enable multi-team configuration
    
    * Fixed the teams configuration
    
    * Minor cleanup in startup.cs
    
    * Wired up GitHub authentication through ASP.NET Core Identity middleware
    
    * Removed unnecessary user-interaction page rendering from authentication workflow, as only GitHub provider is supported.
    
    * Parking changes to use User teams mappings store
    
    * Fixed the authentication and added logged-in user info to the layout.
    
    * Removed unused API from Index page model
    
    * Integrated Google Chart js libraries through BurndownChart view
    
    * minor css style fix
    
    * Fixed the javascript ajax post call to the service
    
    * Added a data layer project and partiallly implemented the teams data manager
    
    * Storing changes
    
    * Second set of changes to enable multi-team configuration
    
    * Integrated Google Chart js libraries through BurndownChart view
    
    * minor css style fix
    
    * Fixed the javascript ajax post call to the service

---
+++ src/MilestoneTracker/Controllers/BurndownController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MilestoneTracker.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilestoneTracker.Controllers
+{
+    [Route("api/Burndown")]
+    public class BurndownController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> GetBurndownDataAsync([FromQuery]string teamName, [FromBody]IEnumerable<string> milestones)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new InvalidOperationException(ModelState.Values.First().Errors.First().ErrorMessage);
+            }
+
+            string milestone = milestones.FirstOrDefault();
+            WorkDTO[] burnDownDatas = new[] {
+                new WorkDTO { Date = new DateTime(2018, 01, 01), Milestone = milestone, DaysOfWorkLeft = 10 },
+                new WorkDTO { Date = new DateTime(2018, 01, 02), Milestone = milestone, DaysOfWorkLeft = 9 },
+                new WorkDTO { Date = new DateTime(2018, 01, 03), Milestone = milestone, DaysOfWorkLeft = 10},
+                new WorkDTO { Date = new DateTime(2018, 01, 04), Milestone = milestone, DaysOfWorkLeft = 8 },
+                new WorkDTO { Date = new DateTime(2018, 01, 06), Milestone = milestone, DaysOfWorkLeft = 7},
+                new WorkDTO { Date = new DateTime(2018, 01, 08), Milestone = milestone, DaysOfWorkLeft = 5 }};
+            await Task.CompletedTask;
+            return new JsonResult(burnDownDatas);
+        }
+    }
+}
