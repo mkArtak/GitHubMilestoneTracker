f46ca805ea3037824d9f9deb2b843e4d9d234577
Artak Mkrtchyan <artak@outlook.com>
2018-03-28 23:26:32 -0700

    Removed unused property from the Member entity

--- src/MT.DataManagement.Teams.AzureSql/Model/Member.cs
+++ src/MT.DataManagement.Teams.AzureSql/Model/Member.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace MT.DataManagement.Teams.AzureSql.Model
+﻿namespace MT.DataManagement.Teams.AzureSql.Model
 {
     public class Member
     {
         public string MemberId { get; set; }
-
-        //public IEnumerable<Team> Teams { get; set; }
     }
 }
