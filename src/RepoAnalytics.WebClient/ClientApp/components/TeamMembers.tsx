5ac09a6c69f67adadfabd27d20c9c024baa087cf
Artak Mkrtchyan <artak@outlook.com>
2018-04-28 19:10:51 -0700

    Finalized the changes in TeamMembers component. Ready to wire-up with backend APIs.

--- src/RepoAnalytics.WebClient/ClientApp/components/TeamMembers.tsx
+++ src/RepoAnalytics.WebClient/ClientApp/components/TeamMembers.tsx
@@ -7,13 +7,14 @@ interface ITeamMembersProps {
     members: ITeamMember[];
 }
 
-interface IteamMembersState {
+interface ITeamMembersState {
     members: ITeamMember[];
     loading: boolean;
     newMemberName: string;
+    canAddCurrentMember: boolean;
 }
 
-export class TeamMembers extends React.Component<ITeamMembersProps, IteamMembersState> {
+export class TeamMembers extends React.Component<ITeamMembersProps, ITeamMembersState> {
     constructor(props: ITeamMembersProps) {
         super(props);
 
@@ -21,39 +22,72 @@ export class TeamMembers extends React.Component<ITeamMembersProps, IteamMembers
         this.addMember = this.addMember.bind(this);
         this.removeMember = this.removeMember.bind(this);
         this.onNewMemberNameChanged = this.onNewMemberNameChanged.bind(this);
-        
+
         this.state = {
             members: props.members,
             loading: false,
-            newMemberName: ""
+            newMemberName: "",
+            canAddCurrentMember: false
         };
     }
 
-    addMember(e: any) {
+    private addMember(e: any) {
         console.log("Adding member " + this.state.newMemberName);
+        if (this.state.newMemberName === "" || this.memberExists(this.state.newMemberName))
+            return;
+
         this.setState({
-            // Call the controller ADD method here passing in the member alias here
-            members: this.state.members.concat([{ name: this.state.newMemberName, includeInReports: false }])
-        }, () => {
-            console.log("Added member");
+            // TODO: Call the controller ADD method here passing in the member alias here
+            members: this.state.members.concat([{ name: this.state.newMemberName, includeInReports: false }]),
+            newMemberName: "",
+            canAddCurrentMember: false
         });
     }
 
-    removeMember(member: ITeamMember, e: any) {
+    private removeMember(member: ITeamMember, e: any) {
         console.log("Removing member " + member.name);
+        var memberIndex = this.getMemberIndex(member.name);
+        if (memberIndex === -1) {
+            return;
+        }
+
+        // TODO: Call the API to remove the member from the current team
+        this.setState({
+            members: this.state.members.filter((_, i) => i != memberIndex)
+        }, () => {
+            console.log("Removed member " + member.name);
+        });
     }
 
-    onNewMemberNameChanged(event: any) {
-        this.setState({ newMemberName: event.target.value });
+    private onNewMemberNameChanged(event: any) {
+        let newName = event.target.value;
+        this.setState({
+            newMemberName: newName,
+            canAddCurrentMember: !this.memberExists(newName)
+        });
+    }
+
+    private memberExists(memberName: string) {
+        return this.getMemberIndex(memberName) != -1;
+    }
+
+    private getMemberIndex(memberName: string) {
+        for (var i = 0; i < this.state.members.length; i++) {
+            if (this.state.members[i].name === memberName) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     public render() {
-        let nodes = this.state.members.map(function (member: ITeamMember) {
+        let nodes = this.state.members.map(member => {
             return (
                 <div className="row" key={member.name}>
                     <div className="col-sm-9">{member.name}</div>
                     <div className="col-sm-1">
-                        <input type="button" className="btn btn-warning" value="remove" />
+                        <input type="button" className="btn btn-warning" value="remove" onClick={(e) => this.removeMember(member, e)} />
                     </div>
                 </div>
             );
@@ -73,7 +107,7 @@ export class TeamMembers extends React.Component<ITeamMembersProps, IteamMembers
                             <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
                             <div className="inline-inputs">
                                 <input type="text" className="form-control" placeholder="Member name" value={this.state.newMemberName} onChange={this.onNewMemberNameChanged} />
-                                <input type="button" className="btn-success" value="Add" onClick={this.addMember} />
+                                <input type="button" disabled={!this.state.canAddCurrentMember} className="btn-success" value="Add" onClick={this.addMember} />
                             </div>
                         </div>
                     </div>
